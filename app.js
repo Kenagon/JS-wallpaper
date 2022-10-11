2975ffcf3372d5c73949bfd40647cc1211fb782f
Kenagon <69736681+Kenagon@users.noreply.github.com>
2022-10-11 15:11:32 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
+  const anchor = document.getElementById('anchor');
+  const rect = anchor.getBoundingClientRect();
+  const anchorX = rect.left + rect.width / 2;
+  const anchorY = rect.top + rect.height / 2;
+  const eye = document.getElementById('eye');
+
 document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
-
-    const anchor = document.getElementById('anchor');
-    const rect = anchor.getBoundingClientRect();
-    const anchorX = rect.left + rect.width / 2;
-    const anchorY = rect.top + rect.height / 2;
-
     const angleDeg = angle(mouseX, mouseY, anchorX, anchorY,);
-
-    console.log(angleDeg);
-
-    const eye = document.getElementById('eye');
     eye.style.transform = `rotate(${90+ angleDeg}deg)`;
 })
 
